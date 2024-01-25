d1003fdfe29db8fa7d31fca4092602a36980b26c
Klimek Jakub <jakub.klimek@krakweb.pl>
2024-01-25 13:43:39 +0100

    change text

--- src/components/Hero/Hero.ui.ts
+++ src/components/Hero/Hero.ui.ts
@@ -2,7 +2,7 @@ const HeroUi = {
     pl: {
         name: "Nazywam się", 
         iam: "i jestem",
-        summary: 'Od ponad dwóch lat aktywnie zbieram doświadczenie pracując nad produktami komercyjnymi.',
+        summary: 'Od ponad trzech lat aktywnie zbieram doświadczenie pracując nad produktami komercyjnymi.',
         contact: "Skontaktuj się",
         hi: "Cześć,",
         developer: "Fullstack Developerem",
@@ -11,7 +11,7 @@ const HeroUi = {
     en: {
         name: "My name is", 
         iam: "and I am",
-        summary: "For over two years I have been actively collecting experience by working on commercial products.",
+        summary: "For over three years I have been actively collecting experience by working on commercial products.",
         contact: "Contact me",
         hi: "Hi,",
         developer: "Fullstack Developer",
